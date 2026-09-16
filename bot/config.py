30da0f1e9b9742280ee8432cbe4c40e6ce8fbd38
rajeshtehriya9267-lgtm <rajeshtehriya9267@gmail.com>
2026-09-16 08:24:36 +0530

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+
+OWNER_ID = int(os.getenv("OWNER_ID"))
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+MONGO_URI = os.getenv("MONGO_URI")
