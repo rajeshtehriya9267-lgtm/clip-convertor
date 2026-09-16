ac6d74b6506af1eb9484049faa34b5be1c6d4a87
rajeshtehriya9267-lgtm <rajeshtehriya9267@gmail.com>
2026-09-16 08:29:39 +0530

    main.py

--- bot/main.py
+++ bot/main.py
@@ -3,7 +3,9 @@ from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 
 from bot.config import BOT_TOKEN
+
 from bot.handlers.start import router as start_router
+from bot.handlers.video import router as video_router
 
 bot = Bot(
     token=BOT_TOKEN,
@@ -15,6 +17,7 @@ bot = Bot(
 dp = Dispatcher()
 
 dp.include_router(start_router)
+dp.include_router(video_router)
 
 async def main():
     await dp.start_polling(bot)
